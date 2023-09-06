756ffb5ab9e27306260bc7d7f4d5277f7ee33900
wern0531 <wern780531@hotmail.com>
2023-09-06 23:15:05 +0800

    修改base路徑

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/Web-layout-week6_vite_tailwind/",
+  base: "/Web-layout-week8_vite_tailwind/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
